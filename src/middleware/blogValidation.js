e3075249ec4a84356f137e6bc514ac57418eaf88
Robertisimwe <robertishimwe0@gmail.com>
2022-02-02 12:38:19 +0200

    added read Me file

--- src/middleware/blogValidation.js
+++ src/middleware/blogValidation.js
@@ -1,13 +1,14 @@
 //validation
-const joi = require('@hapi/joi');
+// const joi = require('@hapi/joi');
+import joi from '@hapi/joi';
 
 
 //article validation
 const articleValidation = (reqBody) =>{
 const articleSchema = {
-    title:joi.string().trim().min(6).required(),
-    articleBody:joi.string().trim().required(),
-    ImageLink:joi.string().trim().min(4).required(),
+    title:joi.string().min(6).required(),
+    articleBody:joi.string().required(),
+    ImageLink:joi.string().min(4).required(),
 }
  return joi.validate(reqBody, articleSchema);
 }
--- src/middleware/commentValidation.js
+++ src/middleware/commentValidation.js
@@ -1,6 +1,6 @@
 const commentValidation = (reqBody) =>{
     const CommentSchema = {
-        userEmail:joi.string().trim().min(3).required(),
+        userEmail:joi.string().min(3).required(),
     }
      return joi.validate(reqBody, CommentSchema);
     }
