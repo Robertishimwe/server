5fd251a422bfe1eb9d39d3a5c8ddcc7bc9b0bf28
Robertisimwe <robertishimwe0@gmail.com>
2022-02-09 19:36:52 +0200

    fixed cors issues

--- src/app.js
+++ src/app.js
@@ -14,13 +14,13 @@ import documentation from '../documentation';
 import swaggerJsDoc from 'swagger-jsdoc';
 import cors from 'cors';
 // app.use(cors())
-// const corsOptions = {
-//     origin: '*',
-//     optionsSuccessStatus: 200 
-const corsOptions = { origin: '*', optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
-} 
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 
+// const corsOptions = { origin: '*', optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+// } 
 
-//   }
+  }
 
 dotenv.config();
 const app = express()
