b1c33bbb51923ee472da423eb8f1584c5bddb1c0
Robertisimwe <robertishimwe0@gmail.com>
2022-02-16 22:14:56 +0200

    fixed cors

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ import cors from 'cors';
      origin: '*',
      optionsSuccessStatus: 200 
  }
-//const corsOptions = { origin: '*', optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+//const corsOptions = { origin: '*', optionsSucessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 //} 
 
 
