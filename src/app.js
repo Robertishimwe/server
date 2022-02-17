7729c96d9bfdcd59ca79697e728e09a054d81270
Robertisimwe <robertishimwe0@gmail.com>
2022-02-17 12:11:48 +0200

    fixed cors

--- src/app.js
+++ src/app.js
@@ -46,7 +46,8 @@ const corsOptions = { origin: '*', optionsSucessStatus: 200 // some legacy brows
 dotenv.config();
 const app = express()
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '80mb'}));
+app.use(express.urlencoded({limit: '80mb'}));
 
 
 
